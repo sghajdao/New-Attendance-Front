0c19110c21fb4230df0024d1372056538d590921
Saad Ghajdaoui Alaoui <saad@Saads-MacBook-Pro.local>
2026-04-28 11:35:47 +0100

    Debugging

--- src/app/components/monitoring/deatails-table/deatails-table.component.ts
+++ src/app/components/monitoring/deatails-table/deatails-table.component.ts
@@ -98,8 +98,9 @@ export class DeatailsTableComponent implements OnInit, OnChanges, OnDestroy {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['searchDto'] && this.searchDto) {
       if (this.searchDto.courses && this.searchDto.courses.length) {
-        this.students = this.buildAggregatedData(this.backup).filter(i => this.searchDto?.courses?.includes('SP26-' + i.crsCde.replaceAll("\\s+", "")))
-        this.rawAttendanceData = this.backup.filter(i => this.searchDto?.courses?.includes('SP26-' + i.crsCde.replaceAll("\\s+", "")))
+        console.log('SP26-' + this.students[0].crsCde.replaceAll("\\s+", ""))
+        this.students = this.students.filter(i => this.searchDto?.courses?.includes('SP26-' + i.crsCde.replaceAll("\\s+", "")))
+        this.rawAttendanceData = this.rawAttendanceData.filter(i => this.searchDto?.courses?.includes('SP26-' + i.crsCde.replaceAll("\\s+", "")))
       }
       if (this.searchDto.seniorities && this.searchDto.seniorities.length) {
         this.students = this.students.filter(i => this.searchDto?.seniorities?.includes(i.seniority))
