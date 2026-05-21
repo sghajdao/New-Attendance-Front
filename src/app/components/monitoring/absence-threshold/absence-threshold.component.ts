859fffc8addc3428fd92d95a5a48a2e34ea10d54
Saad Ghajdaoui Alaoui <saad@192.168.1.54>
2026-05-21 23:26:21 +0100

    Add course in details dialog

--- src/app/components/monitoring/absence-threshold/absence-threshold.component.ts
+++ src/app/components/monitoring/absence-threshold/absence-threshold.component.ts
@@ -365,7 +365,7 @@ export class AbsenceThresholdComponent implements OnInit, OnDestroy {
   }
 
   openAttendanceModal(student: Student) {
-    this.indexeddbService.getByStudentId(student.id, student.course, 'SP').then(records => {
+    this.indexeddbService.getByStudentId(student.id, 'SP').then(records => {
       this.modalRecords = records;
       this.attendanceModal = true;
     });
@@ -564,11 +564,12 @@ export class AbsenceThresholdComponent implements OnInit, OnDestroy {
     }
 
     // Define CSV headers and field mappings
-    const headers = ['Attendance Status', 'Attendance Date', 'Attendance Time'];
+    const headers = ['Attendance Status', 'Attendance Date', 'Attendance Time', 'Course'];
     const rows = this.modalRecords.map(record => [
       record.attendance?.toUpperCase() || '',
       this.formatDateToYYYYMMDD(record.attendanceDate),
-      record.attendanceTime || ''
+      record.attendanceTime || '',
+      record.crsCde
     ]);
 
     // Build CSV content
